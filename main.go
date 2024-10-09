7731be58160554de5dc3c883c22c3301c9bddc1d
AndFran <andrewwilliamfranks@gmail.com>
2024-10-09 20:46:47 +0200

    message

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"log"
 	"time"
 )
 
@@ -15,5 +16,7 @@ type Recipe struct {
 
 func main() {
 	router := gin.Default()
+
+	log.Println("Starting server...")
 	router.Run(":8080")
 }
